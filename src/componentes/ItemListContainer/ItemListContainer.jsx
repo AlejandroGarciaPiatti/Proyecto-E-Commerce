c37de30dc97fcee1c5d1185479cabb0f0b06a96c
Alejandro Garcia Piatti <agarciapiatti@hotmail.com>
2024-05-02 00:25:16 -0300

    pendiente estilos

---
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+const ItemListContainer = ({greeting}) => {
+    
+    return (
+        <div>
+            <h1 id="saludo">{greeting}</h1>
+        </div>
+    )
+}
+export default ItemListContainer
+
---
+++ src/componentes/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import CartWidget from "../CartWidget/CartWidget"
+import "../../App.css"
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>ZeroGames</h3>
+            <div className="secciones">
+                <button>PS5</button>
+                <button>XBOX</button>
+                <button>PC</button>
+                <button>Accesorios</button>
+            </div>
+            <CartWidget className="widget"/>
+        </nav>
+    )
+}
+
+export default NavBar
